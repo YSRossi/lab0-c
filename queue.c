f5f999f4b8f83a819b97664eac0b6887847cddef
YSRossi <cool521525@gmail.com>
2023-02-18 22:33:04 +0800

    Implement q_delete_mid
    There are two pointers that iterated from the head and tail respectively.
    Iterate until they meet or intersect to find the middle node.

--- queue.c
+++ queue.c
@@ -138,6 +138,18 @@ int q_size(struct list_head *head)
 bool q_delete_mid(struct list_head *head)
 {
     // https://leetcode.com/problems/delete-the-middle-node-of-a-linked-list/
+    if (!head || list_empty(head))
+        return false;
+
+    struct list_head *forward = head->next, *backward = head->prev;
+    while (forward != backward && forward->next != backward) {
+        forward = forward->next;
+        backward = backward->prev;
+    }
+    list_del_init(forward);
+    element_t *entry = list_entry(forward, element_t, list);
+    q_release_element(entry);
+
     return true;
 }
 
